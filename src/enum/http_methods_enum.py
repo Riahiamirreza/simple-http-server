2b62a4624f6c1a3ae7d27689ac3dadf4b7746bd3
Amirreza <riahiamirreza79@gmail.com>
2022-04-28 21:43:28 +0430

    Added enum for http-methods.

---
+++ src/enum/http_methods_enum.py
@@ -0,0 +1,20 @@
+from enum import Enum, unique
+
+
+@unique
+class HTTPMethods(Enum):
+    HTTP_GET: str = 'GET'
+    HTTP_POST: str = 'POST'
+    HTTP_PUT: str = 'PUT'
+    HTTP_DELETE: str = 'DELETE'
+
+    @classmethod
+    def get_http_method_by_name(cls, http_method_name: str):
+        if not isinstance(http_method_name, str):
+            raise TypeError
+        
+        for member in cls.__members__.values():
+            if member.value == http_method_name:
+                return member.value
+        else:
+            raise ValueError
