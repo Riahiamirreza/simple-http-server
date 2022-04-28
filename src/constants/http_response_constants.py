4f0ce0f9a86cedf726d04ec9825a120d241bb653
Amirreza <riahiamirreza79@gmail.com>
2022-04-28 16:14:50 +0430

    Added more constants.

--- src/constants/http_response_constants.py
+++ src/constants/http_response_constants.py
@@ -1,6 +1,17 @@
 
 class HTTPResponseConstants:
+    # 2xx
     OK_200: str = 'HTTP/1.1 200 OK'
-    NOT_FOUND_404: str = 'HTTP/1.1 404 Not Found'
-    BAD_REQUEST_400: str = 'HTTP/1.1 400 Bad Request'
+    CREATED_201: str = 'HTTP/1.1 201 Created'
+    ACCEPTED_202: str = 'HTTP/1.1 202 Accepted'
 
+    # 3xx
+    MOVED_PERMANENTLY_301: str = 'HTTP/1.1 301 Moved Permanently'
+
+    # 4xx
+    BAD_REQUEST_400: str = 'HTTP/1.1 400 Bad Request'
+    UNAUTHORIZED_401: str = 'HTTP/1.1 401 Unauthorized'
+    NOT_FOUND_404: str = 'HTTP/1.1 404 Not Found'
+
+    # 5xx
+    INTERNAL_SERVER_ERROR_500: str = 'HTTP/1.1 500 Internal Server Error'
