3a195d940a01719e046e638f8968d7b5a74c6951
Amirreza <riahiamirreza79@gmail.com>
2022-04-28 16:07:46 +0430

    Using constants in http-server.

--- src/constants/http_response_constants.py
+++ src/constants/http_response_constants.py
@@ -1,5 +1,5 @@
 
-class HTTPResponse:
+class HTTPResponseConstants:
     OK_200: str = 'HTTP/1.1 200 OK'
     NOT_FOUND_404: str = 'HTTP/1.1 404 Not Found'
     BAD_REQUEST_400: str = 'HTTP/1.1 400 Bad Request'
--- src/server/http.py
+++ src/server/http.py
@@ -1,6 +1,21 @@
 from src.server.tcp import TCPServer
+from src.constants.http_response_constants import HTTPResponseConstants
 
 
 class HTTPServer(TCPServer):
-    ...
+    LINE_TERMINATOR: bytes = b'\r\n'
+
+    def handle_request(self, data):
+        response_line: str = self._get_response_line(data)
+        headers: dict = ...
+        
+
+    def _get_response_line(self, data) -> str:
+        return HTTPResponseConstants.OK_200
+    
+    def _get_response_headers(self, data) -> dict:
+        ...
+
+    def _get_response_body(self, data) -> str:
+        ...
 
